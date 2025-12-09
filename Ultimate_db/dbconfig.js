cf8990ea046d831436a92b762fe7d18b862f5d1f
yassine Riahi <riahiyassine53@gmail.com>
2025-12-09 01:06:19 +0100

    comit

---
+++ Ultimate_db/dbconfig.js
@@ -0,0 +1,18 @@
+const config = {
+    server: 'MICHAEL',
+    database: 'Ultimate_db',
+    authentification: {
+        option:{
+            userName: 'mika',
+            password: 'mikado'
+        }
+    },
+
+    options: {
+        trustservercertificate: true,
+    }
+
+
+};
+
+module.exports = config;

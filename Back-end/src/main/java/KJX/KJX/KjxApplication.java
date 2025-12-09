5dd067cf0a812cc3ef7743d2a5f5d0f357c2c587
yassine Riahi <riahiyassine53@gmail.com>
2025-12-09 02:00:37 +0100

    comit

---
+++ Back-end/src/main/java/KJX/KJX/KjxApplication.java
@@ -0,0 +1,13 @@
+package KJX.KJX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KjxApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/KJX/KJX/KjxApplicationTests.java
@@ -0,0 +1,13 @@
+package KJX.KJX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

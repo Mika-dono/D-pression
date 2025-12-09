cb526c5f27de9bc7953338c2b35a608d7a38f3f1
yassine Riahi <riahiyassine53@gmail.com>
2025-12-09 02:57:22 +0100

    comit

--- Front-end/src/app/components/shop/shop.component.spec.ts
+++ Front-end/src/app/components/shop/shop.component.spec.ts
@@ -1,20 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ShopComponent } from './shop.component';
 
-import { Shop } from './shop.component';
-
-describe('Shop', () => {
-  let component: Shop;
-  let fixture: ComponentFixture<Shop>;
+describe('ShopComponent', () => {
+  let component: ShopComponent;
+  let fixture: ComponentFixture<ShopComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Shop]
-    })
-    .compileComponents();
+      imports: [ShopComponent]
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(Shop);
+    fixture = TestBed.createComponent(ShopComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
